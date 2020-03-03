601f16dac322d8f17f0a880ff8b6722ae694db2b
Alex Soto <asotobu@gmail.com>
2020-03-03 10:50:36 +0100

    Jib

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -1,24 +1,17 @@
 package org.acme.quickstart;
 
-import java.nio.file.Paths;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/greeting")
 public class GreetingResource {
 
-    @ConfigProperty(name = "greeting.message")
-    String message;
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-
-        return message;
+        return "Hello";
     }
 }
